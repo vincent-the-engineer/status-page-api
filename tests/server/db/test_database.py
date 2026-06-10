3a6905cbf7f69566ac355aede043c4a7d904d92d
vincent-the-engineer <208263664+vincent-the-engineer@users.noreply.github.com>
2026-06-10 13:39:35 +1000

    test: SessionFactory initialisation

--- tests/server/db/test_database.py
+++ tests/server/db/test_database.py
@@ -1,8 +1,13 @@
 import pytest
 from sqlalchemy import Engine
+from sqlalchemy.orm import Session
 
 import server.db.database as db
 
 
 def test_engine():
     assert db.db_engine is not None and isinstance(db.db_engine, Engine)
+
+def test_session_factory():
+    with db.SessionFactory() as session:
+        assert session is not None and isinstance(session, Session)
