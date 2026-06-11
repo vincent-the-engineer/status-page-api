10cab0b76524e9f95f8b1a855f7d69133104840a
vincent-the-engineer <208263664+vincent-the-engineer@users.noreply.github.com>
2026-06-11 23:28:21 +1000

    feat: import User definition

--- src/server/db/migrations/env.py
+++ src/server/db/migrations/env.py
@@ -8,7 +8,7 @@ from sqlalchemy import pool
 from alembic import context
 
 from server.db.schema.base import Base
-
+from server.db.schema.tables.user import User
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
--- tests/server/conftest.py
+++ tests/server/conftest.py
@@ -1,9 +1,13 @@
+from collections.abc import Iterator
 import os
 
 import pytest
 from alembic.config import Config
 from alembic import command
+import sqlalchemy
 from sqlalchemy import create_engine
+from sqlalchemy.engine import Connection
+from sqlalchemy.orm import sessionmaker, Session
 
 import server.db.database as db
 
@@ -27,3 +31,33 @@ def setup_and_teardown_database():
     command.downgrade(alembic_cfg, "base")
     db.db_engine.dispose()
     print("\n[Teardown] Database tables dropped and connections closed.")
+
+@pytest.fixture(scope="function")
+def db_connection() -> Iterator[Connection]:
+    connection = db.db_engine.connect()
+    # Being a top-level transaction.
+    transaction = connection.begin()
+
+    yield connection
+
+    # Roll back the top-level transaction.
+    transaction.rollback()
+    connection.close()
+
+@pytest.fixture(scope="function")
+def db_session(db_connection: Connection) -> Iterator[Session]:
+    Session = sessionmaker(bind=db_connection)
+    session = Session()
+    # Nested transaction savepoint
+    session.begin_nested()
+
+    # If something calls session.commit(), intercept it and start a
+    # new nested transaction.
+    @sqlalchemy.event.listens_for(session, "after_transaction_end")
+    def restart_savepoint(session, transaction):
+        if transaction.nested and not transaction._parent.nested:
+            session.begin_nested()
+
+    yield session
+
+    session.close()
