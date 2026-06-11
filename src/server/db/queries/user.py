d3e247eb4f96826e4f573362c4ae7d7a844acca9
vincent-the-engineer <208263664+vincent-the-engineer@users.noreply.github.com>
2026-06-12 01:00:59 +1000

    feat: add insert_user() function

---
+++ src/server/db/queries/user.py
@@ -0,0 +1,14 @@
+from sqlalchemy.orm import Session
+
+from server.db.schema.tables.user import User
+
+
+def find_user_by_email(session: Session, email: str) -> User:
+    pass
+
+def insert_user(session: Session, email: str, api_key: str) -> User:
+    new_user = User(email=email, api_key=api_key)
+    session.add(new_user)
+    session.commit()
+    session.refresh(new_user)
+    return new_user
