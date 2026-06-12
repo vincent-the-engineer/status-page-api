2b8914faf7e10c95fcab19341d3d9575d356886d
vincent-the-engineer <208263664+vincent-the-engineer@users.noreply.github.com>
2026-06-12 17:31:12 +1000

    test: for insert_user()

---
+++ tests/server/db/queries/test_user.py
@@ -0,0 +1,53 @@
+import pytest
+from sqlalchemy import select, func
+from sqlalchemy.exc import IntegrityError
+
+from server.db.queries.user import (
+    insert_user,
+)
+from server.db.schema.tables.user import User
+
+
+def test_insert_user(db_session):
+    query = select(func.count()).select_from(User)
+    record_count = db_session.scalar(query)
+    assert record_count == 0
+
+    email = "john@example.com"
+    api_key = "abcde"
+    inserted_user = insert_user(db_session, email, api_key)
+    assert inserted_user.id is not None
+    assert inserted_user.email == email
+    assert inserted_user.api_key == api_key
+    assert inserted_user.created_at is not None
+    assert inserted_user.updated_at is not None
+
+    query = select(func.count()).select_from(User)
+    record_count = db_session.scalar(query)
+    assert record_count == 1
+
+    queried_user = db_session.query(User).filter_by(email=email).first()
+    assert queried_user is not None
+    assert queried_user.email == email
+
+def test_insert_user_duplicated_email(db_session):
+    query = select(func.count()).select_from(User)
+    record_count = db_session.scalar(query)
+    assert record_count == 0
+
+    email = "jane@example.org"
+    api_key1 = "abcde"
+    inserted_user = insert_user(db_session, email, api_key1)
+    assert inserted_user.id is not None
+    assert inserted_user.email == email
+    assert inserted_user.api_key == api_key1
+    assert inserted_user.created_at is not None
+    assert inserted_user.updated_at is not None
+
+    api_key2 = "xyz"
+    with pytest.raises(IntegrityError):
+        try:
+            inserted_user2 = insert_user(db_session, email, api_key2)
+        except IntegrityError:
+            db_session.rollback()
+            raise
