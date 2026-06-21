8a1787eb0bcec9548b29701e2393192a8004ea15
vincent-the-engineer <208263664+vincent-the-engineer@users.noreply.github.com>
2026-06-22 09:19:58 +1000

    test: length of returned hash from hash_api_key()

--- tests/server/utils/test_auth.py
+++ tests/server/utils/test_auth.py
@@ -1,9 +1,12 @@
+from server.db.schema.tables.user import SHA256_HASH_LENGTH
 from server.utils.auth import (
     hash_api_key,
 )
 
 
 def test_hash_api_key():
-    assert hash_api_key("test-key") == (
+    hash = hash_api_key("test-key")
+    assert hash == (
         "62af8704764faf8ea82fc61ce9c4c3908b6cb97d463a634e9e587d7c885db0ef"
     )
+    assert len(hash) == SHA256_HASH_LENGTH
