3aa212efa6ff3c090ab420ebbdc7f39ba14a34cd
vincent-the-engineer <208263664+vincent-the-engineer@users.noreply.github.com>
2026-06-25 22:29:56 +1000

    feat: update hash_api_key() to use Argon2

--- src/server/utils/auth.py
+++ src/server/utils/auth.py
@@ -1,5 +1,8 @@
-import hashlib
+from argon2 import PasswordHasher
+from argon2.exceptions import VerifyMismatchError
 
+
+_ph = PasswordHasher()
 
 def hash_api_key(api_key: str) -> str:
-    return hashlib.sha256(api_key.encode()).hexdigest()
+    return _ph.hash(api_key)
