9e0276d7e7ed4849d1b73d47d07f86efb1300d37
vincent-the-engineer <208263664+vincent-the-engineer@users.noreply.github.com>
2026-07-28 00:03:55 +1000

    test: insert_service() with invalid user id

--- tests/server/db/queries/test_service.py
+++ tests/server/db/queries/test_service.py
@@ -1,3 +1,5 @@
+from uuid import uuid4
+
 import pytest
 from sqlalchemy import select, func
 from sqlalchemy.exc import IntegrityError
@@ -32,3 +34,19 @@ def test_insert_service(db_session):
     queried_service = db_session.query(Service).filter_by(user_id=new_user.id).first()
     assert queried_service is not None
     assert queried_service.name == name
+
+def test_insert_service_invalid_user_id(db_session):
+    query = select(func.count()).select_from(Service)
+    record_count = db_session.scalar(query)
+    assert record_count == 0
+
+    email = "john@example.com"
+    api_key = "abcde"
+    new_user = User(email=email, api_key=api_key)
+    db_session.add(new_user)
+    db_session.flush()
+
+    random_id = uuid4()
+    name = "Service 1"
+    with pytest.raises(IntegrityError):
+        inserted_service = insert_service(db_session, random_id, name)
